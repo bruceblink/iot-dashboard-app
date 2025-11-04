d4c4cb9092479ee07f52a39db8b12a63e7b30f71
likanug <likanug.g@qq.com>
2025-11-04 21:42:15 +0800

    chore: Vite启动时增加指定服务端口配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 7654,        // 指定端口
+    strictPort: false,  // 如果端口被占用，自动寻找下一个端口
+    open: true,        // 启动后自动打开浏览器
+  },
 })
